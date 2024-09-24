b58915fffb28b6ac930e7b7e57a2af27df4b34db
tbongkim03 <tbongkim03@gmial.com>
2024-09-24 17:04:53 +0900

    Del b.sh

--- b.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo docker run -d --name mnist-mariadb --env MARIADB_USER=mnist --env MARIADB_PASSWORD=1234 --env MARIADB_DATABASE=mnistdb --env MARIADB_ROOT_PASSWORD=my-secret-pw -p 53306:3306 mariadb:latest
